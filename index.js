eedbb0861446cb4b6c56bf4d63f275fa634e72d6
NgunyiGachie <antogachie@gmail.com>
2024-04-12 13:21:34 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  word = word.toLowerCase(); 
+  let wordArray = word.split("");
+
+  let len = wordArray.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    if (wordArray[i] !== wordArray[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
